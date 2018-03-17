7d2a74123af1f358ae23f30b29f43a74613b6f57
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2018-03-17 20:04:38 +0200

    fixed a bug in symbol table

--- lexical analyzer/SymbolTable.cpp
+++ lexical analyzer/SymbolTable.cpp
@@ -6,14 +6,14 @@
 
 using namespace std;
 
-map<string,string> Symbol_table::symbol_table;
+map<string, string> Symbol_table::symbol_table;
 
-void Symbol_table::add_identifier(string label, string address)
+void Symbol_table::add_identifier(string label, string value)
 {
-    symbol_table[label] = address;
+    symbol_table[label] = value;
 }
 
 bool Symbol_table::search_identifier(string label)
 {
-    return symbol_table[label] != NULL;
+    return symbol_table[label] != "";
 }
--- lexical analyzer/main.cpp
+++ lexical analyzer/main.cpp
@@ -5,7 +5,7 @@
 #include "file_services/FileWriter.h"
 #include "file_services/GrammarReader.h"
 #include "file_services/SourceProgramReader.h"
-
+#include "SymbolTable.h"
 #include <fstream>
 
 using namespace std;
@@ -19,7 +19,8 @@ int main()
 //    s.push_back("Saad");
 //    File_Writer fl;
 //    fl.write("dataa", s);
-
+    Symbol_table::add_identifier("Name", "Ahmed");
+    cout << Symbol_table::search_identifier("Name");
 
 //    Source_program_reader sr;
 //    cout << sr.get_next_char_from_src_program("dataa.lan");
