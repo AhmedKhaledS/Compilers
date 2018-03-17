2b1cd82b0df8e2bccd163a61767f1693d0bcb56e
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2018-03-17 20:00:33 +0200

    Implemented Symbol-Table class

--- lexical analyzer/SymbolTable.cpp
+++ lexical analyzer/SymbolTable.cpp
@@ -3,3 +3,17 @@
 //
 
 #include "SymbolTable.h"
+
+using namespace std;
+
+map<string,string> Symbol_table::symbol_table;
+
+void Symbol_table::add_identifier(string label, string address)
+{
+    symbol_table[label] = address;
+}
+
+bool Symbol_table::search_identifier(string label)
+{
+    return symbol_table[label] != NULL;
+}
--- lexical analyzer/SymbolTable.h
+++ lexical analyzer/SymbolTable.h
@@ -5,4 +5,16 @@
 #ifndef LEXICAL_ANALYZER_SYMBOLTABLE_H
 #define LEXICAL_ANALYZER_SYMBOLTABLE_H
 
+#include <map>
+#include <string>
+
+class Symbol_table
+{
+    private:
+        static std::map<std::string, std::string> symbol_table;
+    public:
+        static void add_identifier(std::string label, std::string value);
+        static bool search_identifier(std::string label);
+};
+
 #endif //LEXICAL_ANALYZER_SYMBOLTABLE_H
