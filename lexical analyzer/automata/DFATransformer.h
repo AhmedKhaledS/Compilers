9e8ad8c661d102570845de57fe21d4d7cca26403
Ahmed Amin <ahmedredaselim@gmail.com>
2018-03-20 18:26:18 +0200

    Design sketch DFA

--- lexical analyzer/automata/DFATransformer.h
+++ lexical analyzer/automata/DFATransformer.h
@@ -5,4 +5,23 @@
 #ifndef LEXICAL_ANALYZER_DFATRANSFORMER_H
 #define LEXICAL_ANALYZER_DFATRANSFORMER_H
 
+#include <vector>
+
+class NFA_DFA_Parser
+{
+
+    private:
+    std::vector<> New_Graph;
+    std::vector<> Grouped_Edges;
+    std::vector<> Grouped_Nodes;
+    void group_Nodes_Crawl(/*takes one node*/);
+    void traverse_And_Group_eps_connected_states();
+
+    public:
+
+    NFA_DFA_Parser(/*vector*/);
+    void build_DFA();
+    std::vector<std::vector> transform_To_DFA();
+
+};
 #endif //LEXICAL_ANALYZER_DFATRANSFORMER_H
--- lexical analyzer/automata/ReducedDFA.h
+++ lexical analyzer/automata/ReducedDFA.h
@@ -5,4 +5,29 @@
 #ifndef LEXICAL_ANALYZER_REDUCEDDFA_H
 #define LEXICAL_ANALYZER_REDUCEDDFA_H
 
+#include <vector>
+
+class Minimize_DFA
+{
+    private:
+    std::vector<> Minimized_Graph;
+    std::vector<> Grouped_Edges;
+    std::vector<> Grouped_Nodes;
+    void group_Nodes_Union(/*takes number of nodes*/);
+    void sift_Through_Graph();
+    bool examine_And_Split_Class(/*takes a class*/);
+
+    public:
+
+    Minimize_DFA(/*vector of vectors*/);
+    void build_min_DFA();
+    std::vector<std::vector> Minimum_DFA();
+
+
+
+
+
+
+};
+
 #endif //LEXICAL_ANALYZER_REDUCEDDFA_H
