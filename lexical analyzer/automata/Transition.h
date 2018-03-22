ac27d66cfbfc25fa890f51c954e3856d60bf7d99
Hisham Osama <hisho_96@yahoo.com>
2018-03-22 15:30:00 +0200

    State and Transition classes added

---
+++ lexical analyzer/automata/Transition.h
@@ -0,0 +1,18 @@
+//
+// Created by hisham on 22/03/18.
+//
+
+#ifndef COMPILERS_TRANSITION_H
+#define COMPILERS_TRANSITION_H
+
+
+class Transition {
+
+    public:
+    int destination;
+    char value;
+
+};
+
+
+#endif //COMPILERS_TRANSITION_H
--- lexical analyzer/main.cpp
+++ lexical analyzer/main.cpp
@@ -8,6 +8,9 @@
 #include "SymbolTable.h"
 #include <fstream>
 
+#include "automata/Transition.h"
+#include "automata/State.h"
+
 using namespace std;
 
 int main()
@@ -24,6 +27,25 @@ int main()
 
 //    Source_program_reader sr;
 //    cout << sr.get_next_char_from_src_program("dataa.lan");
+
+    Transition t_1;
+    t_1.destination = 2;
+    t_1.value = 'a';
+
+    Transition t_2;
+    t_2.destination = 3;
+    t_2.value = '$';
+
+    State s(1);
+    s.add_transition(t_1);
+    s.add_transition(t_2);
+
+    vector<Transition> x = s.get_transitions();
+
+    for (int i = 0; i < x.size(); ++i) {
+        cout << x[i].destination << endl;
+        cout << x[i].value << endl;
+    }
 
     return 0;
 }
