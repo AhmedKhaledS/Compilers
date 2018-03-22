0202f5da3225c9197d0829c9ec15207ddf407e2a
Ahmed Amin <ahmedredaselim@gmail.com>
2018-03-22 07:38:40 +0200

    Fixing the minimization

--- lexical analyzer/automata/DFATransformer.h
+++ lexical analyzer/automata/DFATransformer.h
@@ -10,18 +10,5 @@
 class NFA_DFA_Parser
 {
 
-    private:
-    std::vector<> New_Graph;
-    std::vector<> Grouped_Edges;
-    std::vector<> Grouped_Nodes;
-    void group_Nodes_Crawl(/*takes one node*/);
-    void traverse_And_Group_eps_connected_states();
-
-    public:
-
-    NFA_DFA_Parser(/*vector*/);
-    void build_DFA();
-    std::vector<std::vector> transform_To_DFA();
-
 };
 #endif //LEXICAL_ANALYZER_DFATRANSFORMER_H
--- lexical analyzer/automata/ReducedDFA.h
+++ lexical analyzer/automata/ReducedDFA.h
@@ -6,28 +6,25 @@
 #define LEXICAL_ANALYZER_REDUCEDDFA_H
 
 #include <vector>
+#include <string>
+
 
 class Minimize_DFA
 {
     private:
-    std::vector<> Minimized_Graph;
-    std::vector<> Grouped_Edges;
-    std::vector<> Grouped_Nodes;
-    void group_Nodes_Union(/*takes number of nodes*/);
-    void sift_Through_Graph();
-    bool examine_And_Split_Class(/*takes a class*/);
+    int marked_count;
+    std::vector<std::vector<>> Graph ;
+    std::vector<std::string> States;
+    std::vector<std::string> Final_Unmarked_States;
+    // here we will need a boolean Myhill_Nerode;
+    void Myhill_Nerode_Iteration(int it);
+    void set_up_matrix_bool();
+    void merge_final_states();
 
     public:
 
     Minimize_DFA(/*vector of vectors*/);
-    void build_min_DFA();
     std::vector<std::vector> Minimum_DFA();
-
-
-
-
-
-
 };
 
 #endif //LEXICAL_ANALYZER_REDUCEDDFA_H
