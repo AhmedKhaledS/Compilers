a49a324fe1cddca06e0c96a89f13098370c4a705
Ahmed Amin <ahmedredaselim@gmail.com>
2018-03-22 16:21:28 +0200

    hiding some things

--- lexical analyzer/automata/ReducedDFA.cpp
+++ lexical analyzer/automata/ReducedDFA.cpp
@@ -3,3 +3,29 @@
 //
 
 #include "ReducedDFA.h"
+
+
+Minimize_DFA::Minimize_DFA(std::vector<std::vector<std::string>> dfa_graph)
+{
+    Graph = dfa_graph;
+    set_up_bool_matrix();
+}
+
+void Minimize_DFA::set_up_bool_matrix()
+{
+    int rows = static_cast<int>(Graph.size());
+    int cols = static_cast<int>(Graph[0].size());
+
+    for(int x = 0 ; x <= rows ; x++){
+        for(int y = 0 ; y <= cols; ; y++){
+
+
+
+
+        }
+    }
+
+
+
+
+}
--- lexical analyzer/automata/ReducedDFA.h
+++ lexical analyzer/automata/ReducedDFA.h
@@ -13,17 +13,18 @@ class Minimize_DFA
 {
     private:
     int marked_count;
-    std::vector<std::vector<>> Graph ;
+    std::vector<std::vector<bool>> Marking_Cells;
+    std::vector<std::vector<std::string>> Graph ;
     std::vector<std::string> States;
     std::vector<std::string> Final_Unmarked_States;
     // here we will need a boolean Myhill_Nerode;
     void Myhill_Nerode_Iteration(int it);
-    void set_up_matrix_bool();
+    void set_up_bool_matrix();
     void merge_final_states();
 
     public:
 
-    Minimize_DFA(/*vector of vectors*/);
+    Minimize_DFA(std::vector<std::vector<std::string>> dfa_graph);
     std::vector<std::vector> Minimum_DFA();
 };
 
