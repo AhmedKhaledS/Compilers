943723a95b03f1429a9333f9eafd3f09b35c15b3
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2018-03-22 16:15:03 +0200

    Added implementation for Transition Table

--- lexical analyzer/automata/DFATransformer.h
+++ lexical analyzer/automata/DFATransformer.h
@@ -5,4 +5,18 @@
 #ifndef LEXICAL_ANALYZER_DFATRANSFORMER_H
 #define LEXICAL_ANALYZER_DFATRANSFORMER_H
 
+#include <vector>
+#include <map>
+#include "DFANode.h"
+#include "TransitionTable.h"
+
+class DFATransformer
+{
+
+    private:
+        std::map<int, DFANode> id_to_node;
+    public:
+        DFATransformer();
+        DFANode get_dfa_node(int id);
+};
 #endif //LEXICAL_ANALYZER_DFATRANSFORMER_H
--- lexical analyzer/main.cpp
+++ lexical analyzer/main.cpp
@@ -6,6 +6,7 @@
 #include "file_services/GrammarReader.h"
 #include "file_services/SourceProgramReader.h"
 #include "SymbolTable.h"
+#include "automata/DFATransformer.h"
 #include <fstream>
 
 using namespace std;
@@ -19,11 +20,34 @@ int main()
 //    s.push_back("Saad");
 //    File_Writer fl;
 //    fl.write("dataa", s);
-    Symbol_table::add_identifier("Name", "Ahmed");
-    cout << Symbol_table::search_identifier("Name");
+//    Symbol_table::add_identifier("Name", "Ahmed");
+//    cout << Symbol_table::search_identifier("Name");
 
 //    Source_program_reader sr;
 //    cout << sr.get_next_char_from_src_program("dataa.lan");
+
+    //DFATransformer dt;
+    TransitionTable t;
+    DFANode d_node;
+    d_node.acceptance_state = false;
+    d_node.id = 1;
+    d_node.marked= true;
+    d_node.registered_in_transition_table = true;
+
+    DFANode entry_node;
+    entry_node.acceptance_state = true;
+    entry_node.id = 2;
+    entry_node.marked= true;
+    entry_node.registered_in_transition_table = true;
+    t.add(d_node, 'a', entry_node);
+
+    if (t.search(d_node))
+        cout << "Found \n id= " << t.get_entry(d_node, 'b').id;
+    else
+        cout << "Error \n";
+
+    //cout <<
+
 
     return 0;
 }
