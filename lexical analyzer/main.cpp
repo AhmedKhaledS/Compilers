30a6c3ace7906050a69c144fa09f4918824537e2
AhmedKhaledS <ahmedkhaled1123300@gmail.com>
2018-04-24 19:48:52 +0200

    reformated some files

--- lexical analyzer/main.cpp
+++ lexical analyzer/main.cpp
@@ -2,14 +2,14 @@
 
 #include <string>
 #include <vector>
+#include <fstream>
+
 #include "file_services/FileWriter.h"
 #include "file_services/GrammarReader.h"
 #include "file_services/SourceProgramReader.h"
 #include "LexicalController.h"
 #include "SymbolTable.h"
 #include "automata/DFATransformer.h"
-#include <fstream>
-
 #include "automata/NFAOperations.h"
 #include "automata/NFA.h"
 #include "automata/NFAGenerator.h"
