624975761a9b4d0956d055e7e2432186937d01d3
Hisham Osama <hisho_96@yahoo.com>
2018-04-27 17:58:49 +0200

    Printing parse table

--- parser/TestParseTableStack.cpp
+++ parser/TestParseTableStack.cpp
@@ -1,25 +1,25 @@
+////
+//// Created by ahmed on 4/24/18.
+////
 //
-// Created by ahmed on 4/24/18.
+//#include "ParserTable.h"
 //
-
-#include "ParserTable.h"
-
-void main(){
-
-    // first and follow represented with accordance with Hisham
-
-    ParserTable test_table ;
-    test_table.build_parse_table();
-
-
-
-
-
-
-
-
-
-
-
-
-}
+//void main(){
+//
+//    // first and follow represented with accordance with Hisham
+//
+//    ParserTable test_table ;
+//    test_table.build_parse_table();
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//}
